ba670ea8343af6114fba62b894bc5e558eb7c7bc
Boris <b.edyson.12@gmail.com>
2023-11-28 08:24:23 +0200

    add StackQueue interface and Application class along with a simple program loop

---
+++ src/lab03/Application.java
@@ -0,0 +1,13 @@
+package lab03;
+
+public class Application {
+    private boolean shouldClose;
+    Application() {
+        shouldClose = false;
+    }
+    public void run() {
+        while(!shouldClose) {
+
+        }
+    }
+}
---
+++ src/lab03/StackQueue.java
@@ -0,0 +1,11 @@
+package lab03;
+
+public interface StackQueue {
+    public void push();
+    public void pop();
+    public int top();
+    public int[] getAll();
+    public boolean isEmpty();
+    public boolean isFull();
+
+}
